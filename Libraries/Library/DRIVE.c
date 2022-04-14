7a97da100c76f2ccc80998cae22c5040e8b1e6cf
d1ma <90131808+d1ma11@users.noreply.github.com>
2022-04-14 23:40:15 +0300

    Add files via upload

---
+++ Libraries/Library/DRIVE.c
@@ -0,0 +1,32 @@
+
+#include <bur/plctypes.h>
+#ifdef __cplusplus
+	extern "C"
+	{
+#endif
+	#include "Library.h"
+#ifdef __cplusplus
+	};
+#endif
+/* TODO: Add your comment here */
+void DRIVE(struct DRIVE* inst)
+{
+	UINT mask = inst -> STATE & 0x6f;
+	if(!inst->ENABLE){inst->CMD = CMD_SHUTDOWN;}
+	else
+	{
+		switch(mask)
+		{
+			case STATE_DISABLED:
+				inst->CMD=CMD_SHUTDOWN;
+				break;
+			case STATE_READY:
+				inst->CMD=CMD_ENABLED;
+				break;
+			case STATE_SWITCHED_ON:
+				inst->CMD=CMD_SWITCHED_ON;
+				break;
+		}
+	}
+}
+
